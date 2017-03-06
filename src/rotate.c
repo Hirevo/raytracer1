44a4490f86de1466e67b581408336a0348dd4ef6
Nicolas Polomack <nicolas.polomack@epitech.eu>
2017-03-06 22:11:56 +0100

    Fix mandatory

--- src/rotate.c
+++ src/rotate.c
@@ -5,54 +5,63 @@
 ** Login   <nicolas.polomack@epitech.eu>
 ** 
 ** Started on  Mon Feb 13 21:07:05 2017 Nicolas Polomack
-** Last update Fri Feb 24 14:58:49 2017 Nicolas Polomack
+** Last update Mon Mar  6 22:11:27 2017 Nicolas Polomack
 */
 
 #include <math.h>
 #include <SFML/Graphics.h>
 
-sfVector3f	rx(sfVector3f imp, float r)
+void		rx(sfVector3f *imp, float r)
 {
   sfVector3f	tmp;
 
-  tmp.x = imp.x;
-  tmp.y = imp.y * cos(r) - imp.z * sin(r);
-  tmp.z = imp.y * sin(r) + imp.z * cos(r);
-  return (tmp);
+  if (r)
+    {
+      tmp.x = imp->x;
+      tmp.y = imp->y * cos(r) - imp->z * sin(r);
+      tmp.z = imp->y * sin(r) + imp->z * cos(r);
+      *imp = tmp;
+    }
 }
 
-sfVector3f	ry(sfVector3f imp, float r)
+void		ry(sfVector3f *imp, float r)
 {
   sfVector3f	tmp;
 
-  tmp.y = imp.y;
-  tmp.x = imp.x * cos(r) + imp.z * sin(r);
-  tmp.z = imp.z * cos(r) - imp.x * sin(r);
-  return (tmp);
+  if (r)
+    {
+      tmp.y = imp->y;
+      tmp.x = imp->x * cos(r) + imp->z * sin(r);
+      tmp.z = imp->z * cos(r) - imp->x * sin(r);
+      *imp = tmp;
+    }
 }
 
-sfVector3f	rz(sfVector3f imp, float r)
+void		rz(sfVector3f *imp, float r)
 {
   sfVector3f	tmp;
 
-  tmp.z = imp.z;
-  tmp.x = imp.x * cos(r) - imp.y * sin(r);
-  tmp.y = imp.x * sin(r) + imp.y * cos(r);
-  return (tmp);
+  if (r)
+    {
+      tmp.z = imp->z;
+      tmp.x = imp->x * cos(r) - imp->y * sin(r);
+      tmp.y = imp->x * sin(r) + imp->y * cos(r);
+      *imp = tmp;
+    }
 }
 
 sfVector3f	rotation_xyz(sfVector3f imp, sfVector3f r)
 {
-  imp = rx(imp, r.x * M_PI / 180.0F);
-  imp = ry(imp, r.y * M_PI / 180.0F);
-  imp = rz(imp, r.z * M_PI / 180.0F);
+  rx(&imp, r.x * M_PI / 180.0F);
+  ry(&imp, r.y * M_PI / 180.0F);
+  rz(&imp, r.z * M_PI / 180.0F);
   return (imp);
 }
 
 sfVector3f	rotation_zyx(sfVector3f imp, sfVector3f r)
 {
-  imp = rz(imp, r.z * M_PI / 180.0F);
-  imp = ry(imp, r.y * M_PI / 180.0F);
-  imp = rx(imp, r.x * M_PI / 180.0F);
+  rz(&imp, r.z * M_PI / 180.0F);
+  ry(&imp, r.y * M_PI / 180.0F);
+  rx(&imp, r.x * M_PI / 180.0F);
   return (imp);
 }
